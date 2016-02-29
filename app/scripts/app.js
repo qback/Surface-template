1410a074ce3e91aa57780fe54b1940bf466ebd4d
qback <qbacknn@gmail.com>
2016-02-29 21:46:06 +0300

    feat(scripts): add slider for athlets block

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,28 @@
 import svg4everybody from 'svg4everybody';
 import $ from 'jquery';
+import enquire from 'enquire.js';
+import 'slick-carousel';
+
 
 $(() => {
 	svg4everybody();
+
+	// при ширине viewport < 480px вызываем слайдер
+	enquire.register('screen and (max-width: 479px)', {
+		match() {
+			$('#slider').slick({
+				arrows: false,
+				dots: true,
+				infinite: true,
+				autoplay: true,
+				autoplaySpeed: 6000,
+				speed: 300,
+				slidesToShow: 1
+			});
+		},
+		unmatch() {
+			$('#slider').slick('unslick');
+		}
+
+	});
 });
